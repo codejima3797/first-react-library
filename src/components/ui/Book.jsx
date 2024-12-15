a0ff65c19e286e4f385a9a4239d34c9cddef65d5
Joseph <jgray3797@gmail.com>
2024-12-15 13:19:40 -0600

    first commit

---
+++ src/components/ui/Book.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect, useRef } from "react";
+import Rating from "./Rating";
+import { Link } from "react-router-dom";
+import Price from "./Price";
+
+const Book = ({ book }) => {
+
+  const [img, setImg] = useState();
+
+  const mountedRef = useRef(false);
+
+  useEffect(() => {
+    const image = new Image();
+    image.src = book.url;
+    image.onload = () => {
+      setTimeout(() => {
+        if (mountedRef.current) {
+          setImg(image);
+        }
+      }, 300);
+    };
+    return () => {
+      mountedRef.current = true;
+    };
+  });
+
+
+  return (
+    <div className="book">
+      {!img ? (
+        <>
+          <div className="book__img--skeleton"></div>
+          <div className="skeleton book__title--skeleton"></div>
+          <div className="skeleton book__rating--skeleton"></div>
+          <div className="skeleton book__price--skeleton"></div>
+        </>
+      ) : (
+        <>
+          <Link to={`/books/${book.id}`}>
+            <figure className="book__img--wrapper">
+              <img src={img.src} alt="" className="book__img" />
+            </figure>
+          </Link>
+          <div className="book__title">
+            <Link to={`/books/${book.id}`} className="book__title--link">
+              {book.title}
+            </Link>
+          </div>
+          <Rating rating={book.rating} />
+          <Price
+            originalPrice={book.originalPrice}
+            salePrice={book.salePrice}
+          />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Book;
